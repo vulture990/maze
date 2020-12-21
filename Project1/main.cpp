655b55eb9ccbbd5d6075b1e830a5a8beeaadb30f
Mohamedsalbi <73004418+vulture990@users.noreply.github.com>
2020-12-21 20:41:34 +0100

    drawing each wall makes life easier
    the code is all over the place i'm becaming a code spaghetti master,ineed bit of ordinenless

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,20 +1,21 @@
 #include<SDL.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(int argc, char* argv[])
 {
-	int grid_cell_size = 26;
-	int grid_width = 29;
-	int grid_height = 23;
-
+	int w = 20;
+	int const  grid_width = 29;//ROWS
+	int const grid_height = 23;//cOlUmuns
+	pair <int, int> cell;
 	// + 1 so that the last grid lines fit in the screen.
-	int window_width = (grid_width * grid_cell_size) + 1;
-	int window_height = (grid_height * grid_cell_size) + 1;
-
+	int window_width = (grid_width * w) + 1;
+	int window_height = (grid_height * w) + 1;
+	vector <pair<int,int>> cells;
 	// Dark theme.
 	SDL_Color grid_background = { 22, 22, 22, 255 }; // Barely Black
 	SDL_Color grid_line_color = { 244, 164, 96, 255 } ; // sand
-
+	bool *walls;
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Initialize SDL: %s",
 			SDL_GetError());
@@ -36,8 +37,10 @@ int main(int argc, char* argv[])
 
 	while (!quit) {
 		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if(event.type==SDL_QUIT) {
+		if (SDL_PollEvent(&event)) 
+		{
+			if(event.type==SDL_QUIT) 
+			{
 				quit = SDL_TRUE;
 				break;
 			}
@@ -50,15 +53,42 @@ int main(int argc, char* argv[])
 		// Draw grid lines.
 		SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
 			grid_line_color.b, grid_line_color.a);
-
-		for (int x = 0; x < 1 + grid_width * grid_cell_size;
-			x += grid_cell_size) {
-			SDL_RenderDrawLine(renderer, x, 0, x, window_height);
+		int s = 0;
+		for (int i = 0;i <grid_width+1;i++)
+		{
+			for (int j = 0;j <grid_height+1;j++)
+			{
+				cell = make_pair(i, j);
+				cells.push_back(cell);
+				s++;
+			}
 		}
+		walls = new bool[4];
+		memset(walls, true, sizeof(bool) * 4);//it's going to be set in order way going top,right, bottom,left
+		for (int i = 0;i < s;i++)
+		{
+			pair <int, int> flag;
+			flag = cells[i];
+			int x = flag.first * w;
+			int y = flag.second *w;
+			if (walls[0])
+			{
+				SDL_RenderDrawLine(renderer, x, y, x + w, y);//top
+			}
+			if (walls[1])
+			{
+				SDL_RenderDrawLine(renderer, x + w, y, x + w, y + w);//right
 
-		for (int y = 0; y < 1 + grid_height * grid_cell_size;
-			y += grid_cell_size) {
-			SDL_RenderDrawLine(renderer, 0, y, window_width, y);
+			}
+			if (walls[2])
+			{
+				SDL_RenderDrawLine(renderer, x + w, y + w, x, y + w);//bottom
+			}
+			if (walls[3])
+			{
+				SDL_RenderDrawLine(renderer, x, y + w, x, y);//left
+			}
+			
 		}
 		SDL_RenderPresent(renderer);
 	}
